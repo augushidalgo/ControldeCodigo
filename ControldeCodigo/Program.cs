e41c35e859a8e81d06e00960f70692ebebfae69b
Augusto Hidalgo <augus.hidalgo@gmail.com>
2023-08-27 22:31:50 -0600

    Ahora hay muchos mensajes

--- ControldeCodigo/Program.cs
+++ ControldeCodigo/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
+Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
+Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
+Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
+Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
+Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
