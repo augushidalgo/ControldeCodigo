a3c854651135f434fe022f835dc150f5f5ff56ab
augushidalgo <53703995+augushidalgo@users.noreply.github.com>
2023-08-27 22:36:50 -0600

    Dejar solo un mensaje

--- ControldeCodigo/Program.cs
+++ ControldeCodigo/Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
-Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
-Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
-Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
-Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
-Console.WriteLine("Hola, pruebas en Github. Una modificación más!");
